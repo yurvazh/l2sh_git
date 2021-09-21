21d293b6f92331c5b84a12cb87782e7ab9a97308
yurvazh <90952603+yurvazh@users.noreply.github.com>
2021-09-21 22:01:05 +0300

    Add files via upload

---
+++ work/main.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#define ft first
+#define sd second
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+
+void print_results() {
+    freopen("output.txt", "r", stdin);
+    string fN, lN, book;
+    vector<pair<pair<string, string>, string>> vec(0);
+    set<string> books;
+    int in;
+    cin >> in;
+    if (in == 1) {
+        while (cin >> fN >> lN >> book) {
+            vec.pb(mp(mp(fN, lN), book));
+            books.insert(book);
+        }
+        sort(vec.begin(), vec.end());
+        for (auto t : vec) {
+            cout << t.ft.ft << " " << t.ft.sd << ": " << t.sd << endl;  
+        }
+    } else if (in == 2) {
+        while (cin >> fN >> lN >> book) {
+            vec.pb(mp(mp(lN, fN), book));
+            books.insert(book);
+        }
+        sort(vec.begin(), vec.end());
+        for (auto t : vec) {
+            cout << t.ft.sd << " " << t.ft.ft << ": " << t.sd << endl;  
+        }
+    } else {
+        while (cin >> fN >> lN >> book) {
+            vec.pb(mp(mp(book, fN), lN));
+            books.insert(book);
+        }
+        sort(vec.begin(), vec.end());
+        for (auto t : vec) {
+            cout << t.ft.sd << " " << t.sd << ": " << t.ft.ft << endl;  
+        }
+    }
+    cout << vec.size() << " " << books.size() << endl;
+}
+
+int main() {
+    freopen("questions.txt", "r", stdin);
+    string q1, q2, q3, q4;
+    getline(cin, q1);
+    getline(cin, q2);
+    getline(cin, q3);
+    getline(cin, q4);
+    string fN, lN, book;
+    cout << q1 << " ";
+    int choice;
+    cin >> choice;
+    if (choice == 2) {
+        print_results();
+        return 0;
+    }
+    cout << endl << q2 << " ";
+    cin >> fN;
+    cout << endl << q3 << " ";
+    cin >> lN;
+    cout << endl << q4 << " ";
+    cin >> book;
+    freopen("output.txt", "w", stdout);
+    cout << endl << fN << endl << lN << endl << book << endl;
+}
